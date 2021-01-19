051d821b0ed18f63689afdfdf58d24779557a6a7
rpc08002 <rcurtin0491@gmail.com>
2021-01-18 19:10:14 -0500

    created classes, npm packages

--- Develop/app.js
+++ Develop/app.js
@@ -1,9 +1,18 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// NPM Packages
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+// Output Folder
+const outputPath = path.resolve(__dirname, 'output', "team.html");
+
+// Classes
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const mainHTML = require("./templates/mainHTML")
+
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
