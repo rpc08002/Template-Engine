a4b40c995f622c9cd529abd1d499e1f2a0c45e73
rpc08002 <rcurtin0491@gmail.com>
2021-01-19 16:33:57 -0500

    created functions for template JS files to produce data depending on what card is being filled

--- Develop/templates/engineerhtml.js
+++ Develop/templates/engineerhtml.js
@@ -1,13 +1,19 @@
-<div class="card employee-card">
-    <div class="card-header">
-        <h2 class="card-title">{{ name }}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>{{ role }}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: {{ id }}</li>
-            <li class="list-group-item">Email: <a href="mailto:{{ email }}">{{ email }}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/{{ github }}" target="_blank" rel="noopener noreferrer">{{ github }}</a></li>
-        </ul>
-    </div>
-</div>
+function engineerCard(data) {
+    return `
+      <script src="https://kit.fontawesome.com/7b4d2fea99.js" crossorigin="anonymous"></script>
+          <div class="card employee-card m-2 col-3">
+          <div class="card-header">
+              <h2 class="card-title">${data.name}</h2>
+              <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+          </div>
+          <div class="card-body">
+              <ul class="list-group">
+                  <li class="list-group-item">ID: ${data.id}</li>
+                  <li class="list-group-item">Email: <a href="mailto:${data.email}">${data.email}</a></li>
+                  <li class="list-group-item">GitHub: <a href="https://github.com/${data.github}" target="_blank" rel="noopener noreferrer">${data.github}</a></li>
+              </ul>
+          </div>
+      </div>
+      `;
+  }
+  module.exports = engineerCard;
